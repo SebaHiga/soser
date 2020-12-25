5a0905ceb1ba791fcf2809b4185c869bb971d2f1
Sebastian Higa <seb.higa@outlook.com>
2020-12-25 00:15:59 -0300

    moving detail as a separated header

---
+++ lib/inc/detail.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <type_traits>
+
+namespace sopack :: detail{
+
+template<typename T> concept is_integral = std::is_integral<T>::value;
+template<typename T> concept is_floating = std::is_floating_point<T>::value;
+template<typename T> concept is_numeric = is_integral<T> || is_floating<T>;
+template<typename T> concept is_char_array = std::is_same<T, const char*>::value;
+template<typename T> concept is_char = std::is_same<T, const char>::value;
+template<typename T> concept is_string_class = requires (T str) {str.substr();};
+template<typename T> concept is_string = is_char_array<T> || is_char<T> || is_string_class<T>;
+template<typename T> concept has_sop_serialize = requires (T data) {data._so_serialize();};
+template<typename T> concept has_begin = requires (T data) {data.begin();};
+template<typename T> concept is_container = has_begin<T> && !is_string<T>;
+template<typename T> concept is_so_helper = requires (T data) {data.openBracket();};
+template<typename T> concept not_so_helper = !is_so_helper<T>;
+
+} // namespace
